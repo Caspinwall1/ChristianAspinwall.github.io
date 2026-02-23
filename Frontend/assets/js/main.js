d965ba7a11e4c049ddeadbb346154d20e70616a3
Christian Aspinwall <141345150+Caspinwall1@users.noreply.github.com>
2026-02-22 19:17:47 -0500

    Update main.js

--- Frontend/assets/js/main.js
+++ Frontend/assets/js/main.js
@@ -190,12 +190,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("year").textContent = new Date().getFullYear();
 
-  fetch("https://resume-visitor-api.azurewebsites.net/api/visitcounter")
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("counter").textContent = data.count;
-    })
+ fetch('https://resume-visitor-api-dvbcfjfcgqbyd0g2.canadacentral-01.azurewebsites.net/api/visitorcounterfunction')
+  .then(response => response.text())
+  .then(count => {
+    document.getElementById('visitorCount').innerText = count;
+  });
     .catch(err => console.error("Visitor counter error:", err));
 });
+
 
 
